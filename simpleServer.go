2dc0defe5e38420bfec55f328867e66a8c742a9e
András Czigány <andras.czigany.13@gmail.com>
2019-01-29 11:23:35 +0100

    add basic logging

--- simpleServer.go
+++ simpleServer.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
@@ -17,6 +19,36 @@ func main() {
 	} else {
 		port = ":8080"
 	}
-	log.Println("Server started at http://localhost" + port)
-	log.Fatal(http.ListenAndServe(port, http.FileServer(http.Dir(pwd))))
+	log.Printf("Server started at http://localhost%s serving files from %s\n", port, pwd)
+	log.Fatal(http.ListenAndServe(port, logHandler(http.FileServer(http.Dir(pwd)))))
+}
+
+type ResponseWriter struct {
+	http.ResponseWriter
+	status   int
+	bodySize int
+}
+
+func (w *ResponseWriter) WriteHeader(status int) {
+	w.status = status
+	w.ResponseWriter.WriteHeader(status)
+}
+
+func (w *ResponseWriter) Write(data []byte) (int, error) {
+	w.bodySize += len(data)
+	return w.ResponseWriter.Write(data)
+}
+
+func logHandler(h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		rw := &ResponseWriter{ResponseWriter: w, status: 200}
+		h.ServeHTTP(rw, r)
+		bodySize := rw.Header().Get("Content-Length")
+		if len(bodySize) == 0 {
+			bodySize = strconv.Itoa(rw.bodySize)
+		}
+		log.Printf("Request{url: %s, method: %s} Response{status: %d, content-type: %s, content-length: %s, time: %s}",
+			r.URL.RequestURI(), r.Method, rw.status, rw.Header().Get("Content-Type"), bodySize, time.Since(start))
+	}
 }
